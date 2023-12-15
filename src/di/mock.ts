923e293a809dd23ab6470a24807d4835772967f8
kunlinl <scu2018lkl@gmail.com>
2023-12-15 18:13:55 +0800

    chore: add img to card

--- src/di/mock.ts
+++ src/di/mock.ts
@@ -4,13 +4,13 @@ export interface Artifact {
   type: 'harmonic' | 'gravity' | 'dancing'
   title: string;
   description?: string;
-  // preview: string;
+  preview: string;
 }
 
 export const ArtifactToken = new InjectionToken<Artifact[]>('gallery-list');
 
 export const MOCK_ARTIFACTS: Artifact[] = [
-  {type: 'harmonic', title: 'Harmonic Motion'},
-  {type: 'gravity', title: 'Gravity'},
-  {type: 'dancing', title: 'Dancing Pixels'}
+  {type: 'harmonic', title: 'Harmonic Motion', preview: 'assets/images/harmonic-preview.png'},
+  {type: 'gravity', title: 'Gravity', preview: ''},
+  {type: 'dancing', title: 'Dancing Pixels', preview: ''}
 ]

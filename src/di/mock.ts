0851c908dda2eaa3f96b4ac70c2f1a9ad8efbebd
kunlinl <scu2018lkl@gmail.com>
2023-12-22 14:56:59 +0800

    chore: add raining preview

--- src/di/mock.ts
+++ src/di/mock.ts
@@ -13,5 +13,5 @@ export const MOCK_ARTIFACTS: Artifact[] = [
   {type: 'harmonic', title: 'Harmonic Motion', preview: 'assets/images/harmonic-preview.png'},
   {type: 'gravity', title: 'Gravity', preview: 'assets/images/gravity-preview.png'},
   {type: 'pixel', title: 'Pixels', preview: 'assets/images/image-preview.png'},
-  {type: 'raining', title: 'Raining', preview: 'assets/images/image-preview.png'}
+  {type: 'raining', title: 'Raining', preview: 'assets/images/raining-preview.png'}
 ]
